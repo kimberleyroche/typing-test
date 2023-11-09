461b4c87ce08a9e1ca31981b74727e358b4236c0
Kimberley Roche <kimberley.g.roche@gmail.com>
2023-11-10 04:34:09 +1000

    Add text to be typed

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ let resultsContainer = document.getElementById('results');
 let wpmText = document.getElementById('wpm');
 let accuracyText = document.getElementById('accuracy');
 let timeText = document.getElementById('time');
+
+//lists all the keys we'll count as errors while typing
+const invalidKeys = 'F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 Escape Tab CapsLock Shift Control Alt Meta ArrowLeft ArrowRight ArrowDown ArrowUp Enter'.split(
+    ' ',
+);
+const text =
+    'It is a truth universally acknowledged, that a single man in possession of a good fortune must be in want of a wife. However little known the feelings or views of such a man may be on his first entering a neighbourhood, this truth is so well fixed in the minds of the surrounding families, that he is considered as the rightful property of some one or other of their daughters.';
+
+//string that defines what text we'll add for our typing text 
+const textArr = text.split('');
+const htmlArr = textArr.map((item, index, array) => {
+    if (item === ' ') {
+        return `<span class="space" id="span${index}">${item}</span>`;
+    }
+    return `<span class="char" id="span${index}">${item}</span>`;
+});
+
+let errors = [];
+textContainer.innerHTML = htmlArr.join('');
+let firstTime = true;
+let currentPos = 0;
+let backspaceNeeded = false;
+let currentTime = 0;
+let repeat;
